0aaab58c08af50732a117d1e8130ab03e9e0758a
niklas <niklas@appli.se>
2024-07-26 07:15:14 +0200

    Make the full test work on Vulkan.

--- candle-core/src/vulkan_backend/device.rs
+++ candle-core/src/vulkan_backend/device.rs
@@ -1,5 +1,6 @@
 use super::VulkanError;
 use crate::{DType, Result};
+use bytemuck::Pod;
 use std::sync::Arc;
 use vulkano::buffer::{BufferContents, Subbuffer};
 use vulkano::{
@@ -23,6 +24,78 @@ pub struct VulkanDevice {
 }
 
 impl VulkanDevice {
+    pub(super) fn new_buffer_with_data<T>(&self, data: &[T]) -> Result<Arc<Buffer>>
+    where
+        T: BufferContents + Pod + Sync + Send + Clone,
+    {
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(self.device.clone()));
+
+        let cpu_buffer = Buffer::from_iter(
+            memory_allocator.clone(),
+            BufferCreateInfo {
+                // Specify that this buffer will be used as a transfer source.
+                usage: BufferUsage::TRANSFER_SRC,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                // Specify use for upload to the device.
+                memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                    | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+                ..Default::default()
+            },
+            data.iter().cloned(),
+        )
+        .map_err(VulkanError::ValidatedAllocateBufferError)?;
+
+        let gpu_buffer = Buffer::new_slice::<T>(
+            memory_allocator.clone(),
+            BufferCreateInfo {
+                // Specify use as a storage buffer and transfer destination.
+                usage: BufferUsage::STORAGE_BUFFER
+                    | BufferUsage::TRANSFER_SRC
+                    | BufferUsage::TRANSFER_DST,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                // Specify use by the device only.
+                memory_type_filter: MemoryTypeFilter::PREFER_DEVICE,
+                ..Default::default()
+            },
+            data.len() as DeviceSize,
+        )
+        .map_err(VulkanError::ValidatedAllocateBufferError)?;
+
+        // Copy data from the CPU buffer to the Vulkan buffer
+        let mut builder = AutoCommandBufferBuilder::primary(
+            &StandardCommandBufferAllocator::new(
+                self.device.clone(),
+                StandardCommandBufferAllocatorCreateInfo::default(),
+            ),
+            self.queue.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .map_err(VulkanError::ValidatedVulkanError)?;
+
+        builder
+            .copy_buffer(CopyBufferInfo::buffers(
+                cpu_buffer.clone(),
+                gpu_buffer.clone(),
+            ))
+            .map_err(VulkanError::ValidationError)?;
+        let command_buffer = builder.build().map_err(VulkanError::ValidatedVulkanError)?;
+
+        let future = command_buffer
+            .execute(self.queue.clone())
+            .map_err(VulkanError::CommandBufferExecError)?;
+        future
+            .then_signal_fence_and_flush()
+            .unwrap()
+            .wait(None)
+            .map_err(VulkanError::ValidatedVulkanError)?;
+
+        Ok(gpu_buffer.buffer().clone())
+    }
+
     pub(super) fn allocate_buffer(&self, size: usize, value: u32) -> Result<Arc<Buffer>> {
         let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(self.device.clone()));
         let allocator_create_info = StandardCommandBufferAllocatorCreateInfo::default();
