cf8b68ea2b3b677690b5256c4d7116dfdd34b211
niklas <niklas@appli.se>
2025-03-04 13:46:05 +0100

    Fixed the clamp tests

--- candle-core/src/tensor.rs
+++ candle-core/src/tensor.rs
@@ -2056,8 +2056,12 @@ impl Tensor {
                 (Storage::Cpu(storage), Device::Metal(metal)) => {
                     Storage::Metal(metal.storage_from_cpu_storage(storage)?)
                 }
+                (Storage::Cpu(storage), Device::Vulkan(vulkan)) => {
+                    Storage::Vulkan(vulkan.storage_from_cpu_storage(storage)?)
+                }
                 (Storage::Cuda(storage), Device::Cpu) => Storage::Cpu(storage.to_cpu_storage()?),
                 (Storage::Metal(storage), Device::Cpu) => Storage::Cpu(storage.to_cpu_storage()?),
+                (Storage::Vulkan(storage), Device::Cpu) => Storage::Cpu(storage.to_cpu_storage()?),
                 (Storage::Cuda(storage), Device::Cuda(cuda)) => {
                     // TODO: Avoid passing through the cpu storage here, especially if the gpu ids
                     // are the same.
