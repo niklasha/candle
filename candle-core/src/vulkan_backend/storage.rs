a9018a1f3de71100ed421e99fe6304996a7573a4
niklas <niklas@appli.se>
2025-03-30 22:25:10 +0200

    use ATOMIC_ADD in scatter_add shader as well.

--- candle-core/src/vulkan_backend/storage.rs
+++ candle-core/src/vulkan_backend/storage.rs
@@ -2025,7 +2025,23 @@ impl BackendStorage for VulkanStorage {
         let pipeline = self
             .device()
             .kernels()
-            .load_pipeline(self.device().device(), &key, None)
+            .load_pipeline(
+                self.device().device(),
+                &key,
+                Some(&[(
+                    "FLOAT32_ATOMIC_ADD",
+                    if self
+                        .device
+                        .device()
+                        .enabled_features()
+                        .shader_buffer_float32_atomic_add
+                    {
+                        "1"
+                    } else {
+                        "0"
+                    },
+                )]),
+            )
             .map_err(VulkanError::from)?;
 
         self.scatter_add_op_impl(layout, index, src, src_layout, &pipeline, dim)
