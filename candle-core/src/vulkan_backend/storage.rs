dbeedb3890a6efb38322cc6feba2c1ee3a8b44c8
niklas <niklas@appli.se>
2025-04-02 00:46:00 +0200

    Add partial F16 support.  Improve index_add and index_select shader provisioning.

--- candle-core/src/vulkan_backend/storage.rs
+++ candle-core/src/vulkan_backend/storage.rs
@@ -2371,6 +2371,7 @@ impl BackendStorage for VulkanStorage {
             (DType::F32, DType::F32) => "f32",
             (DType::I64, DType::I64) => "i64",
             (DType::BF16, DType::BF16) => "bf16",
+            (DType::F16, DType::F16) => "f16",
             _ => todo!("Unsupported dtype combo {:?} {:?}", self.dtype, rhs.dtype),
         };
         let key = format!("{}_{}", B::NAME, suffix);
@@ -2769,6 +2770,7 @@ impl BackendStorage for VulkanStorage {
         let suffix = match (self.dtype, rhs.dtype) {
             (DType::F32, DType::F32) => "f32",
             (DType::BF16, DType::BF16) => "bf16",
+            (DType::F16, DType::F16) => "f16",
             _ => todo!("Unsupported dtype combo {:?} {:?}", self.dtype, rhs.dtype),
         };
         let key = format!("gemm_{}", suffix);
@@ -2795,6 +2797,7 @@ impl BackendStorage for VulkanStorage {
             DType::U32 => "u32",
             DType::I64 => "i64",
             DType::BF16 => "bf16",
+            DType::F16 => "f16",
             _ => todo!("Unsupported dtype {:?}", self.dtype),
         };
         let key = format!("copy_strided_src_{}", suffix);
@@ -2860,6 +2863,7 @@ impl BackendStorage for VulkanStorage {
             DType::U32 => "u32",
             DType::I64 => "i64",
             DType::BF16 => "bf16",
+            DType::F16 => "f16",
             _ => todo!("Unsupported dtype {:?}", self.dtype),
         };
         let key = format!("copy2d_{}", suffix);
